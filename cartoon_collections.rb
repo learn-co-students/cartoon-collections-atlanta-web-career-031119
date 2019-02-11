3ddd5fd09682b642252c3e85ddbf06b2177e27d8
active-object-6703 <nerajno@gmail.com>
2019-02-11 03:24:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,18 @@
-def roll_call_dwarves(arr)# code an argument here
+def roll_call_dwarves(arr)
   # Your code here
-  arr.each_with_index do |x|
-    puts "#{arr.index+1}"+"."+"#{x}"
+  roll_call = []
+  arr.each_with_index do |x, index|
+    index = index+1
+    roll_call.push("#{index}. #{x}")
   end
+  puts roll_call
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_plane(arr)# code an argument here
   # Your code here
+  extra = "!"
+  arr.collect!{|call|call.capitalize+extra}
+  return arr
 end
 
 def long_planeteer_calls# code an argument here
