9bb59fa019d2d406e880c599d15e56ab0bba36f4
Travis Martin <travisemerymartin@gmail.com>
2019-03-03 02:17:36 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect {|item| item.push("!").capitalize}
+  array.collect {|item| item.capitalize << "!"}
 end
 
 def long_planeteer_calls# code an argument here
